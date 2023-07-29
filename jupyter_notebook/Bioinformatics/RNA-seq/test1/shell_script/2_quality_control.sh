aff6529c79a99b607351b343ff39d14a881c0fbf
nab-iak <bankaicn@gmail.com>
2023-07-29 15:12:00 +0900

    Update

--- jupyter_notebook/Bioinformatics/RNA-seq/test1/shell_script/2_quality_control.sh
+++ jupyter_notebook/Bioinformatics/RNA-seq/test1/shell_script/2_quality_control.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 这个步骤主要是生成fastq的测序检验报告，格式为html，点开就能看
+
 for i in {50..69}
     do
     fastqc -o /Volumes/WANs_SSD/WAN/RNA-seq/test1/data/FastQC -t 6 /Volumes/WANs_SSD/WAN/RNA-seq/test1/data/FastQ/SRR136156${i}_1.fastq 
